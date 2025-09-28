1139b52ea85b721e1e0eff823f621d8a85984b54
Saw Ye Marn <yemarnpaing44@gmail.com>
2025-09-28 22:15:28 +0630

    api updated

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 
-
-Route::get('register', [AuthController::class, 'register'])->name('api.register');
+Route::get('/register', [AuthController::class, 'register']);
